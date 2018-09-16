df12b5de1c4e5539ae3f7b77e131cf23968fa9f6
lucoadam <43247609+lucoadam@users.noreply.github.com>
2018-09-16 21:27:28 +0545

    Add files via upload

---
+++ php/setup.php
@@ -0,0 +1,15 @@
+<?php
+#database connection 
+define("DB_SERVER", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "password");
+define("DB_NAME", "blog");
+
+#$con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+
+$page_title="Home Page";
+$site_title="Blog";
+
+$home_name="Shiba Kumar Sapkota";
+$home_pos="Deputy Director, Ministry of Eductation";
+?>
